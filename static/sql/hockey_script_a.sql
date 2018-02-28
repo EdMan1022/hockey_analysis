4a8246c7dcde34b2dd1c37dc620a033980532f67
EdMan1022 <EdMan1022@gmail.com>
2018-02-28 12:51:07 -0500

    Added in line comment

--- static/sql/hockey_script_a.sql
+++ static/sql/hockey_script_a.sql
@@ -2,7 +2,7 @@ USE hockey;
 SELECT
   coach.year,
   coach.coach_id,
-  ANY_VALUE(coach.w)
+  ANY_VALUE(coach.w) -- ANY_VALUE is needed to include non-aggregate columns in a group by statement
 FROM coach
 GROUP BY coach.year, coach.coach_id
 ORDER BY coach.year DESC, coach.w DESC;
