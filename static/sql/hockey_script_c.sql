14fed73dd696c68e18daa57cda441ebe767bf98d
Edward <EdMan1022@gmail.com>
2018-02-28 10:25:08 -0500

    Added to final script

--- static/sql/hockey_script_c.sql
+++ static/sql/hockey_script_c.sql
@@ -1,6 +1,10 @@
 USE hockey;
+DROP VIEW IF EXISTS hockey_view_c;
+CREATE VIEW hockey_view_c AS
 SELECT award_player.player_id, award_player.year, COUNT(award_player.award) FROM award_player
 GROUP BY award_player.player_id, award_player.year
-ORDER BY COUNT(award_player.award) DESC
+ORDER BY COUNT(award_player.award) DESC;
 
+SELECT * FROM hockey_view_c LEFT JOIN (master)
+  ON (hockey_view_c.player_id=master.player_id)
 ;
