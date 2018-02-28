fc0389eac363970dbf9bc7d09c5fe04d13ce022d
EdMan1022 <EdMan1022@gmail.com>
2018-02-28 16:29:26 -0500

    Condensed the multiple scripts loading the data into `load_hockey_data`, and added in line comments

--- load_hockey_data.py
+++ load_hockey_data.py
@@ -1,33 +1,34 @@
+import os
 
 import dotenv
-import os
 import sqlalchemy
 
 from etl_script import load_from_pandas_df
 
+# First loads database uri from environmental variable
+# If the user has a .env file in the root, then loads the environmental variable from it
 env_path = '.env'
-
 if os.path.isfile(env_path):
     dotenv.load_dotenv(env_path)
 
-db_host = os.environ.get('DB_HOST')
-db_user = os.environ.get('DB_USER')
-db_pwd = os.environ.get('DB_PASSWORD')
-db = os.environ.get('DB')
-
+# Initializes the engine connecting to the database
 db_uri = os.environ.get('DB_URI')
 mysqldb = sqlalchemy.create_engine(db_uri)
 
 file_tables = [
-    {"file": "static/csv/Master.csv", "table": "master"},
-    {"file": "static/csv/Coaches.csv", "table": "coach"},
-    {"file": "static/csv/AwardsPlayers.csv", "table": "award_player"},
-    {"file": "static/csv/ScoringShootout.csv", "table": "scoring_shootout"},
+    {"file": "static/csv/Master.csv", "table": "master", "columns": None},
+    {"file": "static/csv/Coaches.csv", "table": "coach", "columns": None},
+    {"file": "static/csv/AwardsPlayers.csv", "table": "award_player", "columns": None},
+    {"file": "static/csv/ScoringShootout.csv", "table": "scoring_shootout", "columns": None},
+    {"file": "static/csv/Scoring.csv", "table": "scoring", "columns": ['player_id', 'year', 'tm_id']}
 ]
 
+# For each table to load in, run the load in function with the table name,
+# file path to the csv data, and list of columns to load in
 for file_table in file_tables:
     load_from_pandas_df(
         con=mysqldb,
         csv_path=file_table['file'],
-        table=file_table['table']
+        table=file_table['table'],
+        columns=file_table['columns']
     )
