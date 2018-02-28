a036e5a1e6553ea6d57dea4627196222ef58c373
Edward <EdMan1022@gmail.com>
2018-02-28 10:17:25 -0500

    Started final script

---
+++ static/sql/hockey_script_c.sql
@@ -0,0 +1,6 @@
+USE hockey;
+SELECT award_player.player_id, award_player.year, COUNT(award_player.award) FROM award_player
+GROUP BY award_player.player_id, award_player.year
+ORDER BY COUNT(award_player.award) DESC
+
+;
