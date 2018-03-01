80bb8b5d078caf95e09c189071f14a4299d09a56
Edward <EdMan1022@gmail.com>
2018-02-28 19:36:12 -0500

    Wrapped second order by clause in ANY_VALUE function

--- static/sql/hockey_script_a.sql
+++ static/sql/hockey_script_a.sql
@@ -5,4 +5,4 @@ SELECT
   ANY_VALUE(coach.w) -- ANY_VALUE is needed to include non-aggregate columns in a group by statement
 FROM coach
 GROUP BY coach.year, coach.coach_id
-ORDER BY coach.year DESC, coach.w DESC;
+ORDER BY coach.year DESC, ANY_VALUE(coach.w) DESC;
