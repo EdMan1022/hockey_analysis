86b3df663d604406a31f3b599465f6d08a8a9456
Edward <EdMan1022@gmail.com>
2018-02-28 19:54:41 -0500

    Reformatted sql files

--- static/sql/hockey_script_b.sql
+++ static/sql/hockey_script_b.sql
@@ -1,9 +1,13 @@
 USE hockey;
 DELIMITER $$
-DROP PROCEDURE  IF EXISTS hockey_proc_b$$
+DROP PROCEDURE IF EXISTS hockey_proc_b$$
 CREATE PROCEDURE hockey_proc_b()
   BEGIN
-    SELECT award_player.player_id, award_player.year, COUNT(award_player.award) FROM award_player
+    SELECT
+      award_player.player_id,
+      award_player.year,
+      COUNT(award_player.award)
+    FROM award_player
     GROUP BY award_player.player_id, award_player.year
     ORDER BY COUNT(award_player.award) DESC;
   END$$
